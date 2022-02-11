40b8db2fb3d9661a36e1a0962faabade386cbe91
Marek Roslik <marek.roslik@gmail.com>
2022-02-11 10:13:17 +0300

    Initial Commit

---
+++ CachingImagesAPI/CachingImagesAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CachingImagesAPIApp.swift
+//  CachingImagesAPI
+//
+//  Created by Marek Roslik on 11.02.22.
+//
+
+import SwiftUI
+
+@main
+struct CachingImagesAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
